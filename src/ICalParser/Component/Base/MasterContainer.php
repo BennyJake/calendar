774a91ea4eecd2f0d528e1b268f55cf55e9c379b
Ben Chrisman <me@bennyjake.com>
2016-02-15 22:48:34 -0600

    sample ical parsed and sorted correctly, based on minimal tests

---
+++ src/ICalParser/Component/Base/MasterContainer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 2/15/2016
+ * Time: 9:20 PM
+ */
+
+namespace phpCalendar\Component\Base;
+
+
+class MasterContainer
+{
+
+}
---
+++ src/ICalParser/Debug/Fault/ParseFault.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ben
+ * Date: 2/14/2016
+ * Time: 7:29 PM
+ */
+
+namespace phpCalendar\Debug\Fault;
+
+
+class ParseFault
+{
+
+}
